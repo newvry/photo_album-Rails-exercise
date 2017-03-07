7637a4ad8866c4403d6fcbc23d0fa4623bcac6a6
LYC <LYC@LYCdeMacBook-Air.local>
2017-03-07 11:45:44 +0800

    create Show

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,29 @@
+class PhotosController < ApplicationController
+
+	def index
+		@photos = Photo.all
+ 	end
+
+
+ 	def new
+ 		@photo = Photo.new
+	end
+
+	def create
+ 		@photo = Photo.new(params[:photo])
+ 		@photo.save
+
+		redirect_to photos_url
+	end
+
+	def show
+		@photo = Photo.find(params[:id])
+	end
+
+	private
+
+	def photo_params
+	 params.require(:photo).permit(:title, :date, :description, :file_location)
+	end
+
+end
